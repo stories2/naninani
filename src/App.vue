9840118f6b4272ff5c54ec4dfa83ccc80e88d140
Kim HyunWoo <stories2@naver.com>
2022-05-09 19:52:04 +0900

    asdfa

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <v-app>
+    <v-app-bar
+      color="primary"
+      density="compact"
+      v-bind:image="appBarBackground"
+    >
+      <!-- <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template> -->
+
+      <v-app-bar-title style="color: black">NaniNani</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon style="color: black">
+        <v-icon>mdi-account-circle</v-icon>
+        <!-- <v-icon icon="mdi-home" />
+        <v-icon icon="fas fa-plus" /> -->
+      </v-btn>
+    </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
@@ -14,7 +33,7 @@ export default defineComponent({
 
   data() {
     return {
-      //
+      appBarBackground: require("@/assets/appbarTitle2.jpg"),
     };
   },
 });
