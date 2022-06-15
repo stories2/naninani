48d62390e34b652aa4d6d8c793df79f6f527f016
Kim HyunWoo <stories2@naver.com>
2022-06-15 20:10:19 +0900

    Update Quiz.model.ts

--- src/interfaces/Quiz.model.ts
+++ src/interfaces/Quiz.model.ts
@@ -3,3 +3,56 @@ export interface QuizModel {
   url: string;
   name: string;
 }
+
+type Permission = "public" | "private" | "onlyMe";
+type Lang = "kr | en | jp";
+
+type QuizData = {
+  qid: string;
+  imgUrl: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  answer: string;
+};
+
+type QuizLang = {
+  default: boolean;
+  lang: Lang;
+  title: string;
+  description: string;
+};
+
+type QuizComment = QuizData & {
+  player: PlayerInfo;
+  text: string;
+  createDatetime: Date;
+  isDeleted: boolean;
+  deleteDatetime: Date;
+};
+
+type QuizLike = {
+  player: PlayerInfo;
+  createDatetime: Date;
+};
+
+type PlayerInfo = {
+  uid: string;
+  nickname: string;
+  email: string;
+};
+
+export type QuizInfo = {
+  permission: Permission;
+  player: PlayerInfo;
+  createDatetime: Date;
+  isDeleted: boolean;
+  deleteDatetime: Date;
+  info: QuizLang[];
+  data: QuizData[];
+  comments: number;
+  comment: QuizComment[];
+  likes: number;
+  like: QuizLike[];
+};
