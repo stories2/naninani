fd082c296e6ae19daeed3667422b6567d17eb60a
Kim HyunWoo <stories2@naver.com>
2022-06-17 01:48:44 +0900

    asdf

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,6 +23,15 @@ const routes: Array<RouteRecordRaw> = [
       import(/* webpackChunkName: "about" */ "../views/QuestionView.vue"),
   },
   {
+    path: "/quiz/:quizId",
+    name: "quiz",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/QuizView.vue"),
+  },
+  {
     path: "/result",
     name: "result",
     // route level code-splitting
---
+++ src/views/QuizView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col>hi</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "QuizView",
+
+  mounted() {
+    console.log(this.$route.params.quizId);
+  },
+});
+</script>
+
+<style scoped></style>
