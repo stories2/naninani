cd1f6cef431b1808933ea2b6a09567ebba8361e4
Kim HyunWoo <stories2@naver.com>
2022-06-17 02:19:59 +0900

    Update quiz-dao.ts

--- src/lib/quiz-dao.ts
+++ src/lib/quiz-dao.ts
@@ -1,4 +1,4 @@
-import { QuizInfo } from "@/interfaces/Quiz.model";
+import { QuizData, QuizInfo } from "@/interfaces/Quiz.model";
 import { FirebaseApp, getApp } from "firebase/app";
 import {
   Firestore,
@@ -7,6 +7,8 @@ import {
   query,
   where,
   getDocs,
+  getDoc,
+  doc,
 } from "firebase/firestore";
 
 export class QuizDao {
@@ -21,14 +23,37 @@ export class QuizDao {
     const quizList: QuizInfo[] = [];
     const q = query(collection(this.firestore, "quiz"));
     const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
+    querySnapshot.forEach((document) => {
       // doc.data() is never undefined for query doc snapshots
       //   console.log(doc.id, " => ", doc.data());
       quizList.push({
-        ...doc.data(),
-        docID: doc.id,
+        ...document.data(),
+        docID: document.id,
       } as QuizInfo);
     });
     return quizList;
+  }
+
+  async getQuizData(quizID: string) {
+    const docRef = doc(this.firestore, "quiz", quizID);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const quizData = {
+        ...docSnap.data(),
+        docID: quizID,
+      } as QuizInfo;
+
+      const dataRef = query(collection(this.firestore, `quiz/${quizID}/data`));
+      const dataSnapshot = await getDocs(dataRef);
+
+      dataSnapshot.forEach((document) => {
+        quizData.data.push({
+          ...document.data(),
+        } as QuizData);
+      });
+      return quizData;
+    } else {
+      throw new Error(`quiz id ${quizID}'s data undefined`);
+    }
   }
 }
