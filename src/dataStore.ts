5de860df3fc9b314a784431ea6a2c4919cdc6032
Kim HyunWoo <stories2@naver.com>
2022-06-16 14:16:27 +0900

    Update dataStore.ts

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,58 +1,12 @@
 import { State } from "vue";
 import { createStore } from "vuex";
-import {
-  PlayerInfo,
-  QuizInfo,
-  QuizLang,
-  QuizData,
-  QuizComment,
-  QuizLike,
-} from "./interfaces/Quiz.model";
 
 // Create a new store instance.
 export const store = createStore({
   state() {
     return {
       correctCnt: 0,
-      quizList: [
-        {
-          permission: "public",
-          player: {
-            email: "test@example.com",
-            nickname: "tester",
-            uid: "asdf",
-          } as PlayerInfo,
-          createDatetime: new Date(),
-          isDeleted: false,
-          deleteDatetime: null,
-          maxCorrection: 8,
-          info: [
-            {
-              default: true,
-              lang: "kr",
-              title: "홀로라이브 버튜버 이름 맞추기",
-              description: "JP, EN, ID",
-            } as QuizLang,
-            {
-              default: false,
-              lang: "en",
-              title: "Guess Hololive V-tuber name",
-              describe: "JP, EN, ID",
-            },
-          ] as QuizLang[],
-          data: [
-            {
-              qid: "asdfasdfasf",
-              imgUrl: "",
-              answer: "ときのそら",
-            } as QuizData,
-          ] as QuizData[],
-          comments: 1,
-          comment: [] as QuizComment[],
-          likes: 1,
-          like: [] as QuizLike[],
-        } as QuizInfo,
-      ],
+      quizList: [],
     };
   },
   mutations: {
