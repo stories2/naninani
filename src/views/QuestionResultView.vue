8aef0900856f63809d4626b17acf6f603947de04
Kim HyunWoo <stories2@naver.com>
2022-06-17 16:21:54 +0900

    Update QuestionResultView.vue

--- src/views/QuestionResultView.vue
+++ src/views/QuestionResultView.vue
@@ -13,11 +13,17 @@
         {{ correctCnt }} 개 맞혔어요!
       </b-col>
     </b-row>
-    <b-row style="margin-top: 15px">
-      <b-col>
+    <b-row style="margin-top: 15px" align-h="center">
+      <b-col cols="12" lg="2">
+        <next-quiz-button
+          :name="'홈으로'"
+          v-on:nextBtnClicked="onHomeBtnClicked"
+        ></next-quiz-button>
+      </b-col>
+      <b-col cols="12" lg="2">
         <next-quiz-button
           :name="'다시 시작'"
-          v-on:nextBtnClicked="onNextBtnClicked"
+          v-on:nextBtnClicked="onRestartBtnClicked"
         ></next-quiz-button>
       </b-col>
     </b-row>
@@ -27,6 +33,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import NextQuizButton from "@/components/NextQuizButton.vue";
+import { QuizLog } from "@/interfaces/Quiz.model";
 
 export default defineComponent({
   name: "QuestionResultView",
@@ -36,14 +43,23 @@ export default defineComponent({
   },
 
   data() {
-    return {
-      correctCnt: this.$store.state.correctCnt,
-    };
+    return {};
   },
 
   methods: {
-    onNextBtnClicked() {
+    onHomeBtnClicked() {
       this.$router.push("/");
+    },
+    onRestartBtnClicked() {
+      this.$router.back();
+    },
+  },
+
+  computed: {
+    correctCnt(): number {
+      return this.$store.state.quizLogList.filter(
+        (log: QuizLog) => log.answer === log.selected
+      ).length;
     },
   },
 });
